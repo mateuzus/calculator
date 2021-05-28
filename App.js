e511a23bccd2ba7059362105cc1bf03ed861d9cb
Mateus Marcelino <mateusdsmarcelino@gmail.com>
2021-05-28 17:14:01 -0300

    Logica calculadora

--- App.js
+++ App.js
@@ -1,23 +1,66 @@
 import React, {Component} from 'react'
 import {Platform, StyleSheet, Text, View } from 'react-native'
 import Button from './src/components/Button'
+import Display from './src/components/Display'
+
+const initialState = {
+  displayValue: '0',
+  clearDisplay: false,
+  operation: null,
+  values: [0, 0],
+  current: 0
+}
 
 export default class App extends Component{
+
+  state = {...initialState}
+
+  addDigit = n => {
+    if (n === '.' && this.state.displayValue.includes('.')){
+      return
+    }
+    const clearDisplay = this.state.displayValue === '0' || this.state.clearDisplay
+    const currentValue = clearDisplay ? '' : this.state.displayValue
+    const displayValue = currentValue + n
+    this.setState({displayValue, clearDisplay: false})
+
+    if (n !== '.'){
+      const newValue = parseFloat(displayValue)
+      const values = [...this.state.values]
+      values[this.state.current] = newValue
+    }
+  }
+
+  clearMemory = () => {
+    this.setState({displayValue: '0'})
+  }
+
+  setOperation = operation => {
+    
+  }
+
   render (){
     return(
       <View style={styles.container}>
-        <View style={styles.button}>
-          <Button label='1' />
-          <Button label='2' />
-          <Button label='3' />
-          <Button label='4' />
-          <Button label='5' />
-          <Button label='6' />
-          <Button label='7' />
-          <Button label='8' />
-          <Button label='9' />
-          <Button label='AC' />
-          <Button label='0' />
+        <Display value={this.state.displayValue}/>
+        <View style={styles.buttons}>
+          <Button label='AC' triple onClick={this.clearMemory}/>
+          <Button label='/' operation onClick={this.setOperation}/>
+          <Button label='7' onClick={this.addDigit}/>
+          <Button label='8' onClick={this.addDigit}/>
+          <Button label='9' onClick={this.addDigit}/>
+          <Button label='*' operation onClick={this.setOperation}/>
+          <Button label='4' onClick={this.addDigit}/>
+          <Button label='5' onClick={this.addDigit}/>
+          <Button label='6' onClick={this.addDigit}/>
+          <Button label='-' operation onClick={this.setOperation}/>
+          <Button label='1' onClick={this.addDigit}/>
+          <Button label='2' onClick={this.addDigit}/>
+          <Button label='3' onClick={this.addDigit}/>
+          <Button label='+' operation onClick={this.setOperation}/>
+          <Button label='0' double onClick={this.addDigi}/>
+          <Button label='.' onClick={this.addDigit}/>
+          <Button label='=' operation onClick={this.setOperation}/>
         </View>
       </View>
     );
